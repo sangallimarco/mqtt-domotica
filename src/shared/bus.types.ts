82e7729658f04bb1907c339442d5faeb54133a96
Marco Sangalli <marco.sangalli@b3i.tech>
2020-04-15 14:39:23 +0200

    fixing events

--- src/shared/bus.types.ts
+++ src/shared/bus.types.ts
@@ -2,9 +2,10 @@ export enum Topic {
     TEMP1 = 'sensors/temp1',
     TEMP2 = 'sensors/temp2',
     POWER = 'sensors/power',
-    PUMPS_SWITCH = 'buttons/pumps',
-    PROCESS_SWITCH = 'buttons/light',
-    PROCESS_STATUS = 'sensors/light'
+    PROCESS_STATUS = 'sensors/light',
+
+    PUMPS_SWITCH = 'actuators/pumps',
+    PROCESS_SWITCH = 'actuators/light',
 }
 
 export const SensorTopics = [Topic.TEMP1, Topic.TEMP2, Topic.POWER, Topic.PROCESS_STATUS];
@@ -15,11 +16,3 @@ export interface TopicMessage {
     topic: Topic,
     payload: string
 }
-
-// export type TopicReturnMessage<T extends Topic> = Extract<Extract<TopicMessage, { topic: T }>, 'payload'>;
-
-// // define domain type
-// export type TopicTempMessage = TopicGenericMessage<Topic.TEMP1, number>;
-// export type TopicPowerMessage = TopicGenericMessage<Topic.POWER,string>;
-
-// export type TopicMessage = TopicTempMessage | TopicPowerMessage;
