0216a471fdea3e21f5d2540f0f0496c21bae20b5
Marco Sangalli <marco.sangalli@b3i.tech>
2020-04-26 16:08:33 +0200

    using mqtt password

--- src/shared/mqtt.config.ts
+++ src/shared/mqtt.config.ts
@@ -1,8 +1,6 @@
 import { IClientOptions } from "mqtt";
 
 export const MQTTConfigOptions: IClientOptions = {
-    // {
-    //     username: 'ral', 
-    //     password: 'passwd'
-    // }
-}
+        username: process.env.REACT_APP_MQTT_USER, 
+        password: process.env.REACT_APP_MQTT_PASSWD
+};
