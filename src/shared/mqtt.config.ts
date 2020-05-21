282e34150068ed588fb6747362d2510b564ad1ea
Marco Sangalli <marco.sangalli@b3i.tech>
2020-05-21 21:15:51 +0200

    formatting config file

--- src/shared/mqtt.config.ts
+++ src/shared/mqtt.config.ts
@@ -1,17 +1,17 @@
-import { IClientOptions } from "mqtt"
-import { isEmpty } from "lodash"
+import { IClientOptions } from 'mqtt'
+import { isEmpty } from 'lodash'
 
 export const CONFIG_PATH = 'config'
 
 export function getConfigOptions(): IClientOptions {
-        const storedData = window.localStorage.getItem(CONFIG_PATH)
-        return storedData ? JSON.parse(storedData) : {}
+  const storedData = window.localStorage.getItem(CONFIG_PATH)
+  return storedData ? JSON.parse(storedData) : {}
 }
 
 export function storeConfigOptions(options: IClientOptions): void {
-        window.localStorage.setItem(CONFIG_PATH, JSON.stringify(options))
+  window.localStorage.setItem(CONFIG_PATH, JSON.stringify(options))
 }
 
-export function hasValidConfig() : boolean {
-        return !isEmpty(getConfigOptions())
+export function hasValidConfig(): boolean {
+  return !isEmpty(getConfigOptions())
 }
