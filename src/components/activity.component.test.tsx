2dde97dbc35fac667733208e2e6b3018cc865896
Marco Sangalli <marco.sangalli@b3i.tech>
2020-05-22 08:02:02 +0200

    removing unused

--- src/components/activity.component.test.tsx
+++ src/components/activity.component.test.tsx
@@ -1,11 +1,11 @@
 import { faPlug } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { renderHook } from '@testing-library/react-hooks'
 import { shallow } from 'enzyme'
 import React from 'react'
+import { UseMQTT } from '../shared/mqtt.service'
 import { Topic } from '../shared/mqtt.types'
 import { MQTTActivity } from './activity.component'
-import { renderHook, act } from '@testing-library/react-hooks'
-import { UseMQTT } from '../shared/mqtt.service'
 
 describe('MQTTActivity', () => {
   test('should render', () => {
