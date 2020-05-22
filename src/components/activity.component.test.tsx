eaedb5e0f829e756b72ad3a750e7c7ef1f539de7
Marco Sangalli <marco.sangalli@b3i.tech>
2020-05-22 08:02:42 +0200

    chore: using new build image

--- src/components/activity.component.test.tsx
+++ src/components/activity.component.test.tsx
@@ -1,16 +1,12 @@
 import { faPlug } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { renderHook } from '@testing-library/react-hooks'
 import { shallow } from 'enzyme'
 import React from 'react'
-import { UseMQTT } from '../shared/mqtt.service'
 import { Topic } from '../shared/mqtt.types'
 import { MQTTActivity } from './activity.component'
 
 describe('MQTTActivity', () => {
   test('should render', () => {
-    const { result } = renderHook(() => UseMQTT(Topic.POWER))
-
     const wrapper = shallow(<MQTTActivity topic={Topic.POWER} />)
     expect(
       wrapper.contains(<FontAwesomeIcon size="3x" icon={faPlug} />)
