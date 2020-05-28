68eaa2b350e5ae311219fe712ef0d8b4432edcc5
Marco Sangalli <marco.sangalli@b3i.tech>
2020-05-28 07:39:33 +0200

    design tweaks

--- packages/cli/src/components/timeseries.tsx
+++ packages/cli/src/components/timeseries.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { stringToTimeSeries } from '../shared/formatters'
 import { UseMQTT } from '../shared/mqtt.service'
 import { Topic } from '../shared/mqtt.types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons'
 
 export interface TimeSeriesChartProps {
   topic: Topic
@@ -18,17 +20,31 @@ export const MQTTimeSeries: React.FC<TimeSeriesChartProps> = ({
   const { xBounds, yBounds, values } = stringToTimeSeries(message)
 
   return (
-    <Box align="center" direction="column" gap="medium" fill="horizontal">
-      <Text size="large">{label} </Text>
+    <Box
+      align="center"
+      direction="column"
+      gap="medium"
+      fill="horizontal"
+      height="medium"
+      pad="medium"
+    >
+      <Box direction="row" gap="small">
+        <Text size="large" weight="normal">
+          {label}
+        </Text>
+        <Text size="small">
+          <FontAwesomeIcon size="1x" icon={faArrowDown} /> {yBounds[0]}{' '}
+          <FontAwesomeIcon size="1x" icon={faArrowUp} /> {yBounds[1]}
+        </Text>
+      </Box>
       <Stack fill>
         <Box align="center" fill>
           <Chart
             type="area"
             values={values}
-            round
             bounds={[xBounds, yBounds]}
             thickness="xxsmall"
-            size={{ width: 'full' }}
+            size={{ width: 'full', height: 'full' }}
             color="dark-2"
           />
         </Box>
