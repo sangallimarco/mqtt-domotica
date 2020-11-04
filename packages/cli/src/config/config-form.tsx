a9cb693b8e507684a25d4ff921d29475ed3dd93f
Marco Sangalli <sangalli.marco@gmail.com>
2020-11-04 01:53:40 +0100

    fixing form errors

--- packages/cli/src/config/config-form.tsx
+++ packages/cli/src/config/config-form.tsx
@@ -1,10 +1,13 @@
-import { Form, FormField, TextInput, Box, Button } from "grommet"
-import React from "react"
-import { IClientOptions } from "mqtt"
-import { storeConfigOptions } from "../shared/mqtt.config"
+import { Form, FormField, TextInput, Box, Button } from 'grommet'
+import React from 'react'
+import { IClientOptions } from 'mqtt'
+import { storeConfigOptions } from '../shared/mqtt.config'
 
 export const ConfigForm: React.FC = () => {
-  const [value, setValue] = React.useState<IClientOptions>({})
+  const [value, setValue] = React.useState<IClientOptions>({
+    username: '',
+    password: '',
+  })
 
   const handleSubmit = () => {
     storeConfigOptions(value)
@@ -15,17 +18,36 @@ export const ConfigForm: React.FC = () => {
     setValue({ ...value, ...event })
   }
 
+  const { username, password } = value
+
   return (
     <Box justify="center" direction="row" pad="medium">
-      <Box width="medium">
+      <Box
+        width="medium"
+        gap="medium"
+        background="light-1"
+        round={true}
+        pad="small"
+      >
         <Form value={value} onChange={handleChange} onSubmit={handleSubmit}>
           <FormField name="username" label="User">
-            <TextInput id="username" name="username" />
+            <TextInput
+              id="username"
+              name="username"
+              value={username}
+              size="medium"
+            />
           </FormField>
           <FormField name="password" label="Password">
-            <TextInput id="password" name="password" type="password" />
+            <TextInput
+              id="password"
+              name="password"
+              type="password"
+              value={password}
+              size="medium"
+            />
           </FormField>
-          <Box direction="row" gap="medium">
+          <Box direction="row" gap="medium" pad="small" justify="end">
             <Button type="submit" primary label="Submit" />
           </Box>
         </Form>
